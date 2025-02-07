77cccc89a71687e7db2c584bf619e99a6fce813c
jim-ubuntu <jimferguson_3@protonmail.com>
2025-02-07 23:21:04 +0000

    Completed randomiser for computer

--- rps-script.js
+++ rps-script.js
@@ -1,1 +1,16 @@
 console.log("Hello world. :-]")
+
+function getComputerChoice() {
+    let picker_number = Math.random();
+    if (picker_number <= 0.33) {
+        return "Rock";
+    } 
+    else if (picker_number <= 0.66) {
+        return "Paper";
+    }
+    else if (picker_number <= 1) {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice()); 
