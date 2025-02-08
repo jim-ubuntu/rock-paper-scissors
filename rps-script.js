0a446ff2ffdf67c5da6452a2cd79d865a41b352b
jim-ubuntu <jimferguson_3@protonmail.com>
2025-02-08 12:39:45 +0000

    Implemented player choice function and play game function. Score tracking also added. Just needs refining now

--- rps-script.js
+++ rps-script.js
@@ -1,4 +1,5 @@
-console.log("Hello world. :-]")
+computerScore = 0;
+humanScore = 0;
 
 function getComputerChoice() {
     let picker_number = Math.random();
@@ -13,4 +14,45 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice()); 
+function getHumanChoice() {
+    let humanInput = prompt("Enter R for Rock, P for Paper or S for scissors.").toUpperCase();
+    let humanChoice = "";
+    if (humanInput == "R") {
+        return humanChoice = "Rock" ;
+    }
+    else if (humanInput == "P") {
+        return humanChoice = "Paper";
+    }
+    else if (humanInput == "S") {
+        return humanChoice = "Scissors";
+    }
+   
+}
+
+function playGame(computerChoice, humanChoice) {
+    if (humanChoice == computerChoice){
+        console.log("It's a draw.");
+    }
+    else if (humanChoice == "Paper" & computerChoice == "Rock" ) {
+        humanScore += 1;
+        console.log("Player wins!");
+    }
+    else if (humanChoice == "Scissors" & computerChoice == "Paper" ){
+        humanScore += 1;
+        console.log("Player wins!");
+    }
+    else if (humanChoice == "Rock" & computerChoice == "Scissors" ) {
+        humanScore += 1;
+        console.log("Player wins!");
+    }
+    else {
+        computerScore +=1;
+        console.log("Computer wins!");
+    }
+}
+
+compChoice = getComputerChoice();
+playerChoice = getHumanChoice(); 
+console.log("Computer chose " + compChoice +  "\nPlayer chose " + playerChoice)
+playGame(compChoice, playerChoice);
+console.log("Player score:" + humanScore + "\nComputer score:" + computerScore)
