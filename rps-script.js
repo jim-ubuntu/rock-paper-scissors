9006487a06f9336d012f8f43a8a787777f5bd6ab
jim-ubuntu <jimferguson_3@protonmail.com>
2025-02-24 16:48:02 +0000

    Completed the UI for the game.

--- rps-script.js
+++ rps-script.js
@@ -1,5 +1,6 @@
 computerScore = 0;
 humanScore = 0;
+victorMsg = "";
 
 function getComputerChoice() {
     let picker_number = Math.random();
@@ -14,47 +15,80 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanInput = prompt("Enter R for Rock, P for Paper or S for scissors.").toUpperCase();
-    let humanChoice = "";
-    if (humanInput == "R") {
-        return humanChoice = "Rock" ;
-    }
-    else if (humanInput == "P") {
-        return humanChoice = "Paper";
-    }
-    else if (humanInput == "S") {
-        return humanChoice = "Scissors";
-    }
-   
+function rockSelect(){
+    playGame(getComputerChoice(), "Rock");
 }
-
+function paperSelect(){
+    playGame(getComputerChoice(), "Paper");
+}
+function scissorsSelect(){
+    playGame(getComputerChoice(), "Scissors");
+}
 function playGame(computerChoice, humanChoice) {
     if (humanChoice == computerChoice){
-        console.log("It's a draw.");
+        victorMsg = "It's a draw.";
     }
-    else if (humanChoice == "Paper\" & computerChoice == "Rock" ) {
+    else if (humanChoice == "Paper" & computerChoice == "Rock" ) {
         humanScore += 1;
-        console.log("Player wins!");
+        victorMsg = "Player wins!";
     }
     else if (humanChoice == "Scissors" & computerChoice == "Paper" ){
         humanScore += 1;
-        console.log("Player wins!");
+        victorMsg = "Player wins!";
     }
     else if (humanChoice == "Rock" & computerChoice == "Scissors" ) {
         humanScore += 1;
-        console.log("Player wins!");
+        victorMsg = "Player wins!";
     }
     else {
         computerScore +=1;
-        console.log("Computer wins!");
+        victorMsg = "Computer wins!";
+    }
+}
+const gameWinner = document.querySelector("#gamewinner");
+function gameWinnerMsg(){
+    
+    if (humanScore == 5 && computerScore <5) {
+        gameWinner.textContent = "Player wins the game!";
+    }
+    else if (computerScore == 5 && humanScore <5){
+        gameWinner.textContent = "Computer wins the game!";
     }
 }
 
-for (i = 0; i<5; i++){
-compChoice = getComputerChoice();
-playerChoice = getHumanChoice(); 
-console.log("Computer chose " + compChoice +  "\nPlayer chose " + playerChoice)
-playGame(compChoice, playerChoice);
-console.log("Player score:" + humanScore + "\nComputer score:" + computerScore)
-}
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const scoreLine = document.querySelector("#scoreline");
+const victorMessage = document.querySelector("#roundwinner");
+
+const newScorelineMsg = "The scoreline is: ";
+
+
+
+rockButton.addEventListener("click", () => {
+    rockSelect();
+    scoreLine.textContent = newScorelineMsg + "Player " +  humanScore + " - " + computerScore + " Computer";
+    victorMessage.textContent = victorMsg;
+    gameWinnerMsg();
+})
+paperButton.addEventListener("click", () => {
+    paperSelect();
+    scoreLine.textContent = newScorelineMsg + "Player " +  humanScore + " - " + computerScore + " Computer";
+    victorMessage.textContent = victorMsg;
+    gameWinnerMsg();
+})
+scissorsButton.addEventListener("click", () => {
+    scissorsSelect();
+    scoreLine.textContent = newScorelineMsg + "Player " +  humanScore + " - " + computerScore + " Computer";
+    victorMessage.textContent = victorMsg;
+    gameWinnerMsg();
+})
+
+//for (i = 0; i<5; i++){
+//compChoice = getComputerChoice();
+//playerChoice = getHumanChoice(); 
+//console.log("Computer chose " + compChoice +  "\nPlayer chose " + playerChoice)
+//playGame(compChoice, playerChoice);
+//console.log("Player score:" + humanScore + "\nComputer score:" + computerScore)
+
